c003b49ec2aabcb625dcc662f8312c12102e0b08
celiszin <teste153ovo@gmail.com>
2025-05-23 20:00:48 -0300

    1

--- RaiezenFlix/src/app/pages/home/home.component.ts
+++ RaiezenFlix/src/app/pages/home/home.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieApiService } from '../../services/movie-api.service'; 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor (private service: MovieApiService) {}
+
+  bannerResults: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+
+  // Consumode serviço de banner
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      //console.log(result)
+      this.bannerResults = result.results;
+    }) 
+  }
+
+
+
 
 }
--- RaiezenFlix/src/app/services/movie-api.service.ts
+++ RaiezenFlix/src/app/services/movie-api.service.ts
@@ -1,21 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiService {
 
-  constructor(private http: HttpClient) {  }
+  constructor(private http: HttpClient) { }
 
   baseUrl = "https://api.themoviedb.org/3";
   options = {
-    method : "GET",
-    headers:  {
+    method: "GET",
+    headers: {
       accept: 'application/json',
-      Authorization: 'Bearer ,'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2EyYmY0ZmFhZDFiZDhiMmJkN2VjNjQ1OTc5MDRiOSIsIm5iZiI6MTc0NzI2MTIyMS44MTksInN1YiI6IjY4MjUxNzI1ZTFjMDdkNjg4OTJkMjYzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SzSfcvZJw2HKd2mDnLamOjyoSofcYpP-nYWPj0qQkeI,'
     }
   };
+
+  // Banner de Midias da semana 
+  bannerApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/trending/all/weak?language=pt-br`, this.options);
+  }
 
 }
 
